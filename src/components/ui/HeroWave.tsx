f3d756cd624e44f05550618db37ee974e4275ea8
ROSHAN KUMAR SINGH <businessmarketing0002@gmail.com>
2026-02-04 22:41:15 +0530

    Create HeroWave.tsx

---
+++ src/components/ui/HeroWave.tsx
@@ -0,0 +1,57 @@
+import { motion } from "framer-motion";
+
+export const HeroWave = () => {
+  return (
+    <div className="absolute bottom-0 left-0 right-0 w-full overflow-hidden leading-[0] z-20">
+      <div className="relative w-full h-[80px] md:h-[150px]">
+        
+        {/* Back Wave (Slower, Semi-transparent) */}
+        <motion.div
+          className="absolute bottom-0 left-0 w-[200%] h-full opacity-50 z-10"
+          animate={{ x: ["0%", "-50%"] }}
+          transition={{
+            repeat: Infinity,
+            ease: "linear",
+            duration: 15, // Slower speed
+          }}
+        >
+          <svg
+            className="w-full h-full"
+            viewBox="0 0 1440 320"
+            preserveAspectRatio="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill="#ffffff"
+              d="M0,192L48,197.3C96,203,192,213,288,229.3C384,245,480,267,576,250.7C672,235,768,181,864,181.3C960,181,1056,235,1152,234.7C1248,235,1344,181,1392,154.7L1440,128L1440,320L0,320Z"
+            />
+          </svg>
+        </motion.div>
+
+        {/* Front Wave (Faster, Solid White) */}
+        <motion.div
+          className="absolute bottom-0 left-0 w-[200%] h-full z-20"
+          animate={{ x: ["-50%", "0%"] }}
+          transition={{
+            repeat: Infinity,
+            ease: "linear",
+            duration: 10, // Faster speed
+          }}
+        >
+          <svg
+            className="w-full h-full"
+            viewBox="0 0 1440 320"
+            preserveAspectRatio="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill="#ffffff"
+              d="M0,224L48,213.3C96,203,192,181,288,181.3C384,181,480,203,576,224C672,245,768,267,864,261.3C960,256,1056,224,1152,197.3C1248,171,1344,149,1392,138.7L1440,128L1440,320L0,320Z"
+            />
+          </svg>
+        </motion.div>
+
+      </div>
+    </div>
+  );
+};
