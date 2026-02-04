31634eaa0daedc454bd458af575124d7d6a825a5
ROSHAN KUMAR SINGH <businessmarketing0002@gmail.com>
2026-02-04 22:50:43 +0530

    Update HeroWave.tsx

--- src/components/ui/HeroWave.tsx
+++ src/components/ui/HeroWave.tsx
@@ -5,22 +5,9 @@ export const HeroWave = () => {
     <div className="absolute bottom-0 left-0 right-0 w-full overflow-hidden leading-[0] z-20">
       <div className="relative w-full h-[80px] md:h-[150px]">
         
-        {/* Back Wave (Static - Opacity 50%) */}
-        <div className="absolute bottom-0 left-0 w-full h-full opacity-50 z-10">
-          <svg
-            className="w-full h-full"
-            viewBox="0 0 1440 320"
-            preserveAspectRatio="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill="#ffffff"
-              d="M0,192L48,197.3C96,203,192,213,288,229.3C384,245,480,267,576,250.7C672,235,768,181,864,181.3C960,181,1056,235,1152,234.7C1248,235,1344,181,1392,154.7L1440,128L1440,320L0,320Z"
-            />
-          </svg>
-        </div>
-
-        {/* Front Wave (Static - Solid White) */}
+        {/* Front Wave (Static - Solid White) 
+            Removed the "Back Wave" opacity layer to eliminate the grey fill.
+        */}
         <div className="absolute bottom-0 left-0 w-full h-full z-20">
           <svg
             className="w-full h-full"
